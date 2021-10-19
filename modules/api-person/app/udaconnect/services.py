2032cc1463f48c36bfaa9edc92237f0355d275ad
Sanyam Jain <sj02.9626@gmail.com>
2021-10-19 23:21:50 +0530

    final push

--- modules/api-person/app/udaconnect/services.py
+++ modules/api-person/app/udaconnect/services.py
@@ -7,18 +7,8 @@ from app.udaconnect.models import Person
 from app.udaconnect.schemas import PersonSchema
 from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text
-from kafka import KafkaConsumer, KafkaProducer
 from flask import g, request, jsonify
 from flask_cors import CORS
-
-
-# Set up a Kafka producer
-TOPIC_NAME = 'person'
-KAFKA_SERVER = 'localhost:9092'
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-
-logging.basicConfig(level=logging.WARNING)
-logger = logging.getLogger("udaconnect-api-person")
 
 
 class PersonService:
@@ -28,12 +18,6 @@ class PersonService:
         new_person.first_name = person["first_name"]
         new_person.last_name = person["last_name"]
         new_person.company_name = person["company_name"]
-        # Turn order_data into a binary string for Kafka
-        kafka_data = json.dumps(str(new_person)).encode()
-        # Kafka producer has already been set up in Flask context
-        kafka_producer = producer
-        kafka_producer.send("person", kafka_data)
-        print('Kafka Request Successful')
         db.session.add(new_person)
         db.session.commit()
 
